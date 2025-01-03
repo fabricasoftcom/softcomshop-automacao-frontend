cc4f710de50b5e5a5987f73a1a739c200f5987bb
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2025-01-03 18:07:29 -0300

    feature: remove comentários e adiciona custom command para login restaurando a sessão.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,15 +31,32 @@ Cypress.Commands.add('login', () => {
         LoginPage.preencherCredenciais(user.valid.username, user.valid.password);
         LoginPage.clicarLogin();
         cy.contains('Início').should('be.visible')
-        //cy.url().should('include', '/ta-em-ordem')
     });
-    //cy.wait(10000)
+
     cy.get('body').then(($body) => {
-      if ($body.find('.sweet-alert').length) {
-        cy.get('button[class="confirm"]').contains('OK').click();
-      }
+        if ($body.find('.sweet-alert').length) {
+            cy.get('button[class="confirm"]').contains('OK').click();
+        }
     });
-})
+});
+
+Cypress.Commands.add('loginRestoreSession', () => {
+    cy.session('user_session', () => {
+        cy.fixture('users').then((user) => {
+            LoginPage.visit();
+            LoginPage.preencherCredenciais(user.valid.username, user.valid.password);
+            LoginPage.clicarLogin();
+            cy.contains('Início').should('be.visible')
+        });
+    })
+
+    cy.get('body').then(($body) => {
+        if ($body.find('.sweet-alert').length) {
+            cy.get('button[class="confirm"]').contains('OK').click();
+        }
+    });
+});
+
 //temp, navegação do menu lateral:
 //Clica em uma opção do menu lateral
 Cypress.Commands.add('clicarMenu', function(opcaoClick){
