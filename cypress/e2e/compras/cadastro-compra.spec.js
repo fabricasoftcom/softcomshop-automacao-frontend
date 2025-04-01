27f7ea99e599fd1a9926c3f3fcad8635a27a01df
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2025-03-31 21:48:21 -0300

    feat: adicionando teste para o modulo de compras - importação por xml

--- cypress/e2e/compras/cadastro-compra.spec.js
+++ cypress/e2e/compras/cadastro-compra.spec.js
@@ -1,50 +1,33 @@
 import { beforeEach } from 'mocha';
 import CompraPage from '../../support/pages/Compra/CompraPage'
 
+Cypress.on('uncaught:exception', () => {
+    return false
+});
+
 describe('Cadastro de compra', { tags: ["@cadastro-compra-xml", "@regressivo"] }, () => {
     beforeEach(() => {
         cy.loginArmazenandoSessao();
+        CompraPage.acessarPaginaCompra()
     })
 
-    const excluirNFeExistente = () => {
-        CompraPage.acessarPaginaCompra();
-        CompraPage.selecionarNFeImportada();
-        CompraPage.clicarExcluirSelecionados();
-        CompraPage.confirmarExclusao();
-    };
-
-    afterEach(() => {
-        CompraPage.selecionarNFeImportada('4382813');
-        CompraPage.clicarExcluirSelecionados();
-        CompraPage.confirmarExclusao();
+    it('Importando NFe pelo XML', () => {
+        cy.get('#table-form-body > tr > :nth-child(1) > .checkbox > input').check();
+        cy.get('.ibox-content > #btn-excluir-selecionados > .hidden-xs').click();
+        cy.wait(3000)
+        cy.get('body').type('{enter}')
+        cy.get('.btn-info > .hidden-xs').click();
+        cy.get('#option-xml > .block-view-option').click();
+        cy.get('#file').invoke('show').click({force: true}).attachFile('nfe_importacao.xml');
+        cy.get('#btn-importar').click();
+        cy.get('#auto_cfop').clear('1');
+        cy.get('#auto_cfop').type('1102');
+        cy.get('.typeahead-display').click();
+        cy.get('#form-importacao > :nth-child(2) > [style="padding: 0; margin: 0"]').click();
+        cy.get('#btn-importar-xml').click();
+        cy.wait(5000)
+        cy.get('.btn-danger').click()
+        cy.wait(3000)
+        cy.get('body').type('{enter}')
     })
-
-    it('Cadastro de Compra importando XML - Sucesso', () => {
-        const numeroNFe = '4382813';
-
-        excluirNFeExistente(numeroNFe);
-        CompraPage.acessarPaginaCompra();
-        CompraPage.importarNfe();
-        CompraPage.clicarImportacaoXML();
-        CompraPage.clicarAnexarNFe();
-        CompraPage.clicarImportar();
-        CompraPage.informarCFOP();
-        CompraPage.informarVinculoFiscal();
-        CompraPage.importarNFe();
-        CompraPage.validarMensagemSucesso();
-    });
-
-    it('Cadastro de Compra importando XML - Sem informar CFOP', () => {
-        const numeroNFe = '4382813';
-
-        excluirNFeExistente(numeroNFe);
-        CompraPage.importarNfe();
-        CompraPage.acessarPaginaCompra();
-        CompraPage.importarNfe();
-        CompraPage.clicarImportacaoXML();
-        CompraPage.clicarAnexarNFe();
-        CompraPage.clicarImportar();
-        CompraPage.importarNFe();
-        CompraPage.validarMensagemErro();
-    });
 })
